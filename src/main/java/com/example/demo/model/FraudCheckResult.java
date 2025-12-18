6e1ce3803a1cf2655ce027de8adef3ae3e6139b8
bala-amypo <bala@amypo.in>
2025-12-18 06:02:55 +0000

    User commit

--- src/main/java/com/example/demo/model/FraudCheckResult.java
+++ src/main/java/com/example/demo/model/FraudCheckResult.java
@@ -1,64 +1,64 @@
-package com.example.demo.model;
+// package com.example.demo.model;
 
-import jakarta.persistence.*;
+// import jakarta.persistence.*;
 
-@Entity
-public class FraudCheckResult {
+// @Entity
+// public class FraudCheckResult {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    @ManyToOne
-    private Claim claim;
+//     @ManyToOne
+//     private Claim claim;
 
-    @ManyToOne
-    private FraudRule fraudRule;
+//     @ManyToOne
+//     private FraudRule fraudRule;
 
-    private boolean flagged;
+//     private boolean flagged;
 
 
-    public FraudCheckResult() {
-    }
+//     public FraudCheckResult() {
+//     }
 
-    public FraudCheckResult(Claim claim, boolean flagged, FraudRule fraudRule, Long id) {
-        this.claim = claim;
-        this.flagged = flagged;
-        this.fraudRule = fraudRule;
-        this.id = id;
-    }
+//     public FraudCheckResult(Claim claim, boolean flagged, FraudRule fraudRule, Long id) {
+//         this.claim = claim;
+//         this.flagged = flagged;
+//         this.fraudRule = fraudRule;
+//         this.id = id;
+//     }
    
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public Claim getClaim() {
-        return claim;
-    }
+//     public Claim getClaim() {
+//         return claim;
+//     }
 
-    public void setClaim(Claim claim) {
-        this.claim = claim;
-    }
+//     public void setClaim(Claim claim) {
+//         this.claim = claim;
+//     }
 
-    public FraudRule getFraudRule() {
-        return fraudRule;
-    }
+//     public FraudRule getFraudRule() {
+//         return fraudRule;
+//     }
 
-    public void setFraudRule(FraudRule fraudRule) {
-        this.fraudRule = fraudRule;
-    }
+//     public void setFraudRule(FraudRule fraudRule) {
+//         this.fraudRule = fraudRule;
+//     }
 
-    public boolean isFlagged() {
-        return flagged;
-    }
+//     public boolean isFlagged() {
+//         return flagged;
+//     }
 
-    public void setFlagged(boolean flagged) {
-        this.flagged = flagged;
-    }
+//     public void setFlagged(boolean flagged) {
+//         this.flagged = flagged;
+//     }
 
     
-}
+// }
--- src/main/java/com/example/demo/model/FraudRule.java
+++ src/main/java/com/example/demo/model/FraudRule.java
@@ -1,83 +1,83 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import jakarta.persistence.*;
+import jakarta.persistence.*;
 
-// @Entity
-// @Table(uniqueConstraints = @UniqueConstraint(columnNames = "ruleName"))
-// public class FraudRule {
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "ruleName"))
+public class FraudRule {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//     private String ruleName;
-//     private String conditionField; 
-//     private String operator;      
-//     private String value;
-//     private String severity;       
+    private String ruleName;
+    private String conditionField; 
+    private String operator;      
+    private String value;
+    private String severity;       
 
     
 
-//     public FraudRule() {
-//     }
+    public FraudRule() {
+    }
 
-//     public FraudRule(String conditionField, Long id, String operator, String ruleName, String severity, String value) {
-//         this.conditionField = conditionField;
-//         this.id = id;
-//         this.operator = operator;
-//         this.ruleName = ruleName;
-//         this.severity = severity;
-//         this.value = value;
-//     }
+    public FraudRule(String conditionField, Long id, String operator, String ruleName, String severity, String value) {
+        this.conditionField = conditionField;
+        this.id = id;
+        this.operator = operator;
+        this.ruleName = ruleName;
+        this.severity = severity;
+        this.value = value;
+    }
     
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     public String getRuleName() {
-//         return ruleName;
-//     }
+    public String getRuleName() {
+        return ruleName;
+    }
 
-//     public void setRuleName(String ruleName) {
-//         this.ruleName = ruleName;
-//     }
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
 
-//     public String getConditionField() {
-//         return conditionField;
-//     }
+    public String getConditionField() {
+        return conditionField;
+    }
 
-//     public void setConditionField(String conditionField) {
-//         this.conditionField = conditionField;
-//     }
+    public void setConditionField(String conditionField) {
+        this.conditionField = conditionField;
+    }
 
-//     public String getOperator() {
-//         return operator;
-//     }
+    public String getOperator() {
+        return operator;
+    }
 
-//     public void setOperator(String operator) {
-//         this.operator = operator;
-//     }
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
 
-//     public String getValue() {
-//         return value;
-//     }
+    public String getValue() {
+        return value;
+    }
 
-//     public void setValue(String value) {
-//         this.value = value;
-//     }
+    public void setValue(String value) {
+        this.value = value;
+    }
 
-//     public String getSeverity() {
-//         return severity;
-//     }
+    public String getSeverity() {
+        return severity;
+    }
 
-//     public void setSeverity(String severity) {
-//         this.severity = severity;
-//     }
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
 
 
-// }
+}
