e0ae7b9dd7c6be40807261d75b253f8a0cabc07b
bala-amypo <bala@amypo.in>
2025-12-18 16:52:47 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/implement/FraudDetectionImplement.java
@@ -0,0 +1,55 @@
+package com.example.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Claim;
+import com.example.entity.FraudCheckResult;
+import com.example.entity.FraudRule;
+import com.example.repository.ClaimRepository;
+import com.example.repository.FraudCheckResultRepository;
+import com.example.repository.FraudRuleRepository;
+import com.example.service.FraudDetectionService;
+
+@Service
+public class FraudDetectionServiceImpl implements FraudDetectionService {
+
+    @Autowired
+    private ClaimRepository claimRepository;
+
+    @Autowired
+    private FraudRuleRepository fraudRuleRepository;
+
+    @Autowired
+    private FraudCheckResultRepository resultRepository;
+
+    @Override
+    public FraudCheckResult evaluateClaim(Long claimId) {
+
+        Claim claim = claimRepository.findById(claimId).orElse(null);
+        boolean isFraud = false;
+
+        List<FraudRule> rules = fraudRuleRepository.findAll();
+
+        for (FraudRule rule : rules) {
+            
+            if (claim.getClaimAmount() > rule.getThresholdAmount()) {
+                isFraud = true;
+                break;
+            }
+        }
+
+        FraudCheckResult result = new FraudCheckResult();
+        result.setClaim(claim);
+        result.setFraud(isFraud);
+
+        return resultRepository.save(result);
+    }
+
+    @Override
+    public FraudCheckResult getResultByClaim(Long claimId) {
+        return resultRepository.findByClaimId(claimId);
+    }
+}
