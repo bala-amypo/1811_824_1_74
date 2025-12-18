bf79228f61eb2df4d8ca2f461ff549ef3227789c
bala-amypo <bala@amypo.in>
2025-12-18 05:42:55 +0000

    User commit

--- src/main/java/com/example/demo/model/Policy.java
+++ src/main/java/com/example/demo/model/Policy.java
@@ -1,74 +1,74 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import java.time.LocalDate;
+import java.time.LocalDate;
 
-// import jakarta.persistence.*;
+import jakarta.persistence.*;
 
 
-// @Entity
-// @Table(uniqueConstraints=@UniqueConstraint(columnNames="policyNumber"))
-// public class Policy {
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     @ManyToOne
-//     @JoinColumn(name = "user_id")
-//     private User user;
-//     @Column(nullable=false,unique=true)
-//     private String policyNumber;
-//     private String policyType;
-//     private LocalDate startDate;
-//     private LocalDate endDate;
+@Entity
+@Table(uniqueConstraints=@UniqueConstraint(columnNames="policyNumber"))
+public class Policy {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(nullable=false,unique=true)
+    private String policyNumber;
+    private String policyType;
+    private LocalDate startDate;
+    private LocalDate endDate;
 
-//     public Policy() {
-//     }
+    public Policy() {
+    }
 
-//     public Policy(LocalDate endDate, Long id, String policyNumber, String policyType, LocalDate startDate, User user) {
-//         this.endDate = endDate;
-//         this.id = id;
-//         this.policyNumber = policyNumber;
-//         this.policyType = policyType;
-//         this.startDate = startDate;
-//         this.user = user;
-//     }
+    public Policy(LocalDate endDate, Long id, String policyNumber, String policyType, LocalDate startDate, User user) {
+        this.endDate = endDate;
+        this.id = id;
+        this.policyNumber = policyNumber;
+        this.policyType = policyType;
+        this.startDate = startDate;
+        this.user = user;
+    }
      
 
-//     public Long getId() {
-//         return id;
-//     }
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-//     public User getUser() {
-//         return user;
-//     }
-//     public void setUser(User user) {
-//         this.user = user;
-//     }
-//     public String getPolicyNumber() {
-//         return policyNumber;
-//     }
-//     public void setPolicyNumber(String policyNumber) {
-//         this.policyNumber = policyNumber;
-//     }
-//     public String getPolicyType() {
-//         return policyType;
-//     }
-//     public void setPolicyType(String policyType) {
-//         this.policyType = policyType;
-//     }
-//     public LocalDate getStartDate() {
-//         return startDate;
-//     }
-//     public void setStartDate(LocalDate startDate) {
-//         this.startDate = startDate;
-//     }
-//     public LocalDate getEndDate() {
-//         return endDate;
-//     }
-//     public void setEndDate(LocalDate endDate) {
-//         this.endDate = endDate;
-//     }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+    public String getPolicyType() {
+        return policyType;
+    }
+    public void setPolicyType(String policyType) {
+        this.policyType = policyType;
+    }
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 
     
-// }
+}
