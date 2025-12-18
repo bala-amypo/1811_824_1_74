34eb6ea23f2c1d020a586dbd06b2149e50da610f
bala-amypo <bala@amypo.in>
2025-12-18 08:46:29 +0000

    User commit

--- src/main/java/com/example/demo/model/Policy.java
+++ src/main/java/com/example/demo/model/Policy.java
@@ -2,31 +2,20 @@ package com.example.model;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
+
 
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "policyNumber"))
+@Table(uniqueConstraints=@UniqueConstraint(columnNames="policyNumber"))
 public class Policy {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
-
-    @Column(nullable = false, unique = true)
+    @Column(nullable=false,unique=true)
     private String policyNumber;
-
     private String policyType;
     private LocalDate startDate;
     private LocalDate endDate;
@@ -34,8 +23,7 @@ public class Policy {
     public Policy() {
     }
 
-    public Policy(LocalDate endDate, Long id, String policyNumber,
-                  String policyType, LocalDate startDate, User user) {
+    public Policy(LocalDate endDate, Long id, String policyNumber, String policyType, LocalDate startDate, User user) {
         this.endDate = endDate;
         this.id = id;
         this.policyNumber = policyNumber;
@@ -43,55 +31,44 @@ public class Policy {
         this.startDate = startDate;
         this.user = user;
     }
-    
+     
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public User getUser() {
         return user;
     }
-
     public void setUser(User user) {
         this.user = user;
     }
-
     public String getPolicyNumber() {
         return policyNumber;
     }
-
     public void setPolicyNumber(String policyNumber) {
         this.policyNumber = policyNumber;
     }
-
     public String getPolicyType() {
         return policyType;
     }
-
     public void setPolicyType(String policyType) {
         this.policyType = policyType;
     }
-
     public LocalDate getStartDate() {
         return startDate;
     }
-
     public void setStartDate(LocalDate startDate) {
         this.startDate = startDate;
     }
-
     public LocalDate getEndDate() {
         return endDate;
     }
-
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
     }
-    
+
     
 }
