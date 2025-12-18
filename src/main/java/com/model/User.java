f4b553e0f4b2c4f9a543956c5686ff31df75d562
bala-amypo <bala@amypo.in>
2025-12-18 04:46:42 +0000

    User commit

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -1,13 +1,6 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-
+import jakarta.persistence.*;
 @Entity
 @Table(name="users",uniqueConstraints=@UniqueConstraint(columnNames="email"))
 public class User {
