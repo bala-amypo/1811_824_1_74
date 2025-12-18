5ad2f4169b2a95bc28fa6d72feeb3621a0fc7d56
bala-amypo <bala@amypo.in>
2025-12-18 05:52:55 +0000

    User commit

--- src/main/java/com/example/demo/model/Claim.java
+++ src/main/java/com/example/demo/model/Claim.java
@@ -1,83 +1,83 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import jakarta.persistence.*;
-// import java.time.LocalDate;
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
-// @Entity
-// public class Claim {
+@Entity
+public class Claim {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//     @ManyToOne
-//     private Policy policy;
+    @ManyToOne
+    private Policy policy;
 
-//     private LocalDate claimDate;
-//     private Double claimAmount;
-//     private String description;
-//     private String status; 
+    private LocalDate claimDate;
+    private Double claimAmount;
+    private String description;
+    private String status; 
 
 
-//     public Claim() {
-//     }
+    public Claim() {
+    }
     
-//     public Claim(Long id, Policy policy, LocalDate claimDate, Double claimAmount, String description, String status) {
-//         this.id = id;
-//         this.policy = policy;
-//         this.claimDate = claimDate;
-//         this.claimAmount = claimAmount;
-//         this.description = description;
-//         this.status = status;
-//     }
+    public Claim(Long id, Policy policy, LocalDate claimDate, Double claimAmount, String description, String status) {
+        this.id = id;
+        this.policy = policy;
+        this.claimDate = claimDate;
+        this.claimAmount = claimAmount;
+        this.description = description;
+        this.status = status;
+    }
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     public Policy getPolicy() {
-//         return policy;
-//     }
+    public Policy getPolicy() {
+        return policy;
+    }
 
-//     public void setPolicy(Policy policy) {
-//         this.policy = policy;
-//     }
+    public void setPolicy(Policy policy) {
+        this.policy = policy;
+    }
 
-//     public LocalDate getClaimDate() {
-//         return claimDate;
-//     }
+    public LocalDate getClaimDate() {
+        return claimDate;
+    }
 
-//     public void setClaimDate(LocalDate claimDate) {
-//         this.claimDate = claimDate;
-//     }
+    public void setClaimDate(LocalDate claimDate) {
+        this.claimDate = claimDate;
+    }
 
-//     public Double getClaimAmount() {
-//         return claimAmount;
-//     }
+    public Double getClaimAmount() {
+        return claimAmount;
+    }
 
-//     public void setClaimAmount(Double claimAmount) {
-//         this.claimAmount = claimAmount;
-//     }
+    public void setClaimAmount(Double claimAmount) {
+        this.claimAmount = claimAmount;
+    }
 
-//     public String getDescription() {
-//         return description;
-//     }
+    public String getDescription() {
+        return description;
+    }
 
-//     public void setDescription(String description) {
-//         this.description = description;
-//     }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-//     public String getStatus() {
-//         return status;
-//     }
+    public String getStatus() {
+        return status;
+    }
 
-//     public void setStatus(String status) {
-//         this.status = status;
-//     }
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
 
-// }
+}
