8b5451cf92ff19eb0406e9398ba44b81a974837d
bala-amypo <bala@amypo.in>
2025-12-20 16:32:57 +0000

    User commit

--- src/main/java/com/example/demo/service/FraudDetectionService.java
+++ src/main/java/com/example/demo/service/FraudDetectionService.java
@@ -3,14 +3,6 @@ package com.example.demo.service;
 import com.example.demo.model.FraudCheckResult;
 
 public interface FraudDetectionService {
-    FraudCheckResult evaluateClaim(Long claimId);
-    FraudCheckResult getResultByClaim(Long claimId);
-}
-package com.example.demo.service;
-
-import com.example.demo.model.FraudCheckResult;
-
-public interface FraudDetectionService {
 
     FraudCheckResult evaluateClaim(Long claimId);
 
