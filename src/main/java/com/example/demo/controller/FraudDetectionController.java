4e70f46d8b09111268fa6374e95daf4f40361b1b
bala-amypo <bala@amypo.in>
2025-12-21 05:12:01 +0000

    User commit

--- src/main/java/com/example/demo/controller/FraudDetectionController.java
+++ src/main/java/com/example/demo/controller/FraudDetectionController.java
@@ -13,13 +13,13 @@ public class FraudDetectionController {
     @Autowired
     private FraudDetectionService fraudDetectionService;
 
-    
+    // POST /api/fraud-check/evaluate/{claimId}
     @PostMapping("/evaluate/{claimId}")
     public FraudCheckResult evaluateClaim(@PathVariable Long claimId) {
         return fraudDetectionService.evaluateClaim(claimId);
     }
 
-    
+    // GET /api/fraud-check/result/claim/{claimId}
     @GetMapping("/result/claim/{claimId}")
     public FraudCheckResult getResult(@PathVariable Long claimId) {
         return fraudDetectionService.getResultByClaim(claimId);
