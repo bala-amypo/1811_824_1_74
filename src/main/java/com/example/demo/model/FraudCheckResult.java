12a73ae77d1d69e483c37d9c61fc7805d38e5f30
bala-amypo <bala@amypo.in>
2025-12-21 07:59:53 +0000

    User commit

--- src/main/java/com/example/demo/model/FraudCheckResult.java
+++ src/main/java/com/example/demo/model/FraudCheckResult.java
@@ -9,31 +9,17 @@ public class FraudCheckResult {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private boolean fraud;
+
     @ManyToOne
     private Claim claim;
 
-    @ManyToOne
-    private FraudRule fraudRule;
-
-    private boolean flagged;
-
-
-    public FraudCheckResult() {
+    public boolean isFraud() {
+        return fraud;
     }
 
-    public FraudCheckResult(Claim claim, boolean flagged, FraudRule fraudRule, Long id) {
-        this.claim = claim;
-        this.flagged = flagged;
-        this.fraudRule = fraudRule;
-        this.id = id;
-    }
-   
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    public void setFraud(boolean fraud) {
+        this.fraud = fraud;
     }
 
     public Claim getClaim() {
@@ -43,22 +29,5 @@ public class FraudCheckResult {
     public void setClaim(Claim claim) {
         this.claim = claim;
     }
-
-    public FraudRule getFraudRule() {
-        return fraudRule;
-    }
-
-    public void setFraudRule(FraudRule fraudRule) {
-        this.fraudRule = fraudRule;
-    }
-
-    public boolean isFlagged() {
-        return flagged;
-    }
-
-    public void setFlagged(boolean flagged) {
-        this.flagged = flagged;
-    }
-
-    
 }
+
