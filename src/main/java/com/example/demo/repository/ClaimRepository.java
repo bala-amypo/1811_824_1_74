73b7ef181f9b5ef34ea937266566129215e3f851
bala-amypo <bala@amypo.in>
2025-12-25 17:05:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClaimRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Claim;
-
-public interface ClaimRepository extends JpaRepository<Claim,Long>{
-    
-}
--- src/main/java/com/example/demo/service/FraudDetectionService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.FraudCheckResult;
-
-public interface FraudDetectionService {
-    FraudCheckResult checkFraud(Long claimId);
-}
