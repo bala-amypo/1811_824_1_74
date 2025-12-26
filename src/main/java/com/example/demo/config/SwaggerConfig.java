223df67123c46157fa6558171df4aa7c52280b1b
bala-amypo <bala@amypo.in>
2025-12-26 15:49:11 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
                 )
                 // Server configuration (keep only if deployed URL is needed)
                 .servers(List.of(
-                        new Server().url("https://9084.32procr.amypo.ai/")
+                        new Server().url("")
                 ))
                 // JWT Security
                 .addSecurityItem(
