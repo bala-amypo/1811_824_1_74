d4f06c356f506b6d15cb9dea5a6780fd93cea616
bala-amypo <bala@amypo.in>
2025-12-26 16:51:10 +0000

    User commit

--- src/main/java/com/example/demo/model/Policy.java
+++ src/main/java/com/example/demo/model/Policy.java
@@ -38,7 +38,7 @@ public class Policy {
         this.endDate = endDate;
     }
     
-    // Getters and Setters
+   
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     
