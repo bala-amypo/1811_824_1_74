62125e097ead37d6b2c8f215ac808b62a56e3850
bala-amypo <bala@amypo.in>
2025-12-26 15:39:12 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ServletConfig {
+public class SecurityConfig {
 
     @Bean
     public ServletRegistrationBean<HelloServlet> helloServlet() {
