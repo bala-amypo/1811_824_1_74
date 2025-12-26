61bffea3cd55567e797bb1b175690827b2c46afc
bala-amypo <bala@amypo.in>
2025-12-26 06:19:21 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ApiErrorResponse.java
@@ -0,0 +1,44 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+public class ApiErrorResponse {
+
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+
+    public ApiErrorResponse() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ApiErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
